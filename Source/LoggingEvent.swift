9da9bb7789bf6e025c00bdbb0632a729b030e359
Scott Byrns <contact@scottbyrns.com>
2016-04-03 23:16:19 -0600

    Magnets!

--- Source/LoggingEvent.swift
+++ Source/LoggingEvent.swift
@@ -1,11 +1,23 @@
 public struct LoggingEvent {
 
-    public let locationInfo : LocationInfo
-    public let timestamp : Int
-    public let level : Log.Level
-    public let name : String
-    public let logger : Logger
-    public var message : Any?
-    public var error : ErrorProtocol?
-    
+    public let locationInfo: LocationInfo
+    public let timestamp: Int
+    public let level: Log.Level
+    public let name: String
+    public let logger: Logger
+    public var message: Any?
+    public var error: ErrorProtocol?
+
+    init (locationInfo : LocationInfo, timestamp: Int, level: Log.Level, name: String, logger: Logger, message: Any?, error: ErrorProtocol?) {
+
+        self.locationInfo = locationInfo
+        self.timestamp = timestamp
+        self.level = level
+        self.name = name
+        self.logger = logger
+        self.message = message
+        self.error = error
+
+    }
+
 }
