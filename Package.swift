1270b490d4c62cb3470412d3fa05d12994049124
Scott Byrns <contact@scottbyrns.com>
2016-04-03 20:51:22 -0600

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -27,7 +27,7 @@ import PackageDescription
 let package = Package(
     name: "Log",
     dependencies: [
-        .Package(url: "https://github.com/scottbyrns/Data.git", majorVersion: 0, minor: 4),
+        .Package(url: "https://github.com/Zewo/Data.git", majorVersion: 0, minor: 4),
         .Package(url: "https://github.com/VeniceX/Venice.git", majorVersion: 0, minor: 4),
     ]
 )
